4d54178c1026671c52ca2757825ff9d0b1e12464
Pem Tsering Gurung <pemgurung541@gmail.com>
2025-02-28 04:00:14 -0500

    Fix deployment configuration

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 app.use('/recipes', recipesRouter);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
